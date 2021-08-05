3e80f08b1b5b7d1eda34a44063e2827b2ce7a085
micro1985 <micro1985@mail.ru>
2021-08-05 16:01:24 +0300

    added terraform

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  access_key = "---------"
+  secret_key = "---------"
+  region     = "eu-north-1"
+}
+
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0d441f5643da997cb"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+
+  user_data = <<EOF
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install httpd
+echo "${data.local_file.indx.content}" >> /var/www/html/index.html
+sudo service httpd start
+sudo chkconfig httpd on
+EOF
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+#  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+data "local_file" "indx" {
+    filename = "${path.module}/index.html"
+}
