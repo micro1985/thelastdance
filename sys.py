43c14bdfec7591dd181a024085f0c5c051364722
micro1985 <micro1985@mail.ru>
2021-08-05 15:55:35 +0300

    added datetime

--- sys.py
+++ sys.py
@@ -1,9 +1,11 @@
 import platform,socket,getpass
+from datetime import datetime
 
 ost = platform.uname()
 hst = socket.gethostname()
 usr = getpass.getuser()
+dtm = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 
 file = open("index.html", "w")
-file.write("<html>\nOperating system info:"+" "+str(ost)+"\n<br>"+"Hostname:"+" "+str(hst)+"\n<br>"+"Username: "+str(usr)+"\n</html>")
+file.write("<html>\nOperating system info:"+" "+str(ost)+"\n<br>"+"Hostname:"+" "+str(hst)+"\n<br>"+"Username: "+str(usr)+"\n<br>"+"Time on server: "+str(dtm)+"\n</html>")
 file.close()
